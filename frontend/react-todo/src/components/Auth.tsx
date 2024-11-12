0cffac6e31304d9cd38d81884129f76e7219dbfb
izazakuro <izazakuro@gmail.com>
2024-11-13 00:15:07 +0900

    Task Manager Implementation

--- frontend/react-todo/src/components/Auth.tsx
+++ frontend/react-todo/src/components/Auth.tsx
@@ -1,6 +1,6 @@
-import { useState, FormEvent } from "react"
-import {CheckBadgeIcon, ArrowPathIcon} from '@heroicons/react/24/solid'
-import { useMutateAuth } from "../hooks/useMutateAuth"
+import { useState, FormEvent } from "react";
+import {CheckBadgeIcon, ArrowPathIcon} from "@heroicons/react/24/solid";
+import { useMutateAuth } from "../hooks/useMutateAuth";
 
 export const Auth = () => {
     const [email, setEmail] = useState('')
